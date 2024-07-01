34a79afcd116f026a1ce5f2933c5e82f29cb1861
GizmoGear22 <mpsimmer@gmail.com>
2024-07-01 14:35:50 -0400

    Added component form data

--- src/app/post-card-to-api.service.ts
+++ src/app/post-card-to-api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ICardModel } from './Models/CardModel';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostCardToAPIService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  url: string = 'https://localhost:44382/api/AvailableCardsAPI/PostNewCard'
+
+
+  AddCard(card: ICardModel) : Observable<ICardModel>
+  {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    return this.http.post<ICardModel>(this.url, card, httpOptions)
+  }
 }
--- src/app/post-card-view/post-card-view.component.ts
+++ src/app/post-card-view/post-card-view.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ICardModel } from '../Models/CardModel';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { OnInit } from '@angular/core';
+import { PostCardToAPIService } from '../post-card-to-api.service';
 
 @Component({
   selector: 'app-post-card-view',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './post-card-view.component.html',
   styleUrl: './post-card-view.component.css'
 })
-export class PostCardViewComponent {
+export class PostCardViewComponent implements OnInit {
+
+  constructor(private htmlData: PostCardToAPIService, private builder: FormBuilder)
+  {}
+
+  newForm!: FormGroup;
+
+  ngOnInit() {
+    this.newForm = this.builder.group({
+      id: [0],
+      name: [''],
+      cost: 0,
+      attack: 0,
+      defense: 0,
+      inputType: ['']
+
+    })
+  }
+
+  OnSubmit()
+  {
+    const cardData: ICardModel = this.newForm.value;
+    this.htmlData.AddCard(cardData)
+  }
 
 }

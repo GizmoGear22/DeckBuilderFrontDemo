5533ee505dc8356119eb37dd7617dd96e10e98ad
GizmoGear22 <mpsimmer@gmail.com>
2024-07-19 05:10:46 -0400

    Added a couplel urls, and fixed list

--- src/app/card-data.service.ts
+++ src/app/card-data.service.ts
@@ -11,11 +11,12 @@ export class CardDataService {
   constructor(private http: HttpClient) { }
 
   cardData!: Observable<ICardModel[]>
-  htmlString: string = "https://deckbuildapi.azurewebsites.net/api/AvailableCardsAPI/GetAllCards";
+  htmlString1: string = "https://deckbuildapi.azurewebsites.net/api/AvailableCardsAPI/GetAllCards";
+  htmlString2: string = "https://localhost:44351/api/AvailableCardsAPI/GetAllCards";
 
   GetAllCardData() : Observable<ICardModel[]>
   {
-    this.cardData = this.http.get<ICardModel[]>(this.htmlString)
+    this.cardData = this.http.get<ICardModel[]>(this.htmlString2)
     return this.cardData;
   }
 }

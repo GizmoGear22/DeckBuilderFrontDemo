4f96a5b46fa38e1df480cd8d806f5aa82e43799d
GizmoGear22 <mpsimmer@gmail.com>
2024-07-24 15:41:16 -0400

    Fixed url links part 2

--- src/app/get-cards-by-type.service.ts
+++ src/app/get-cards-by-type.service.ts
@@ -16,7 +16,7 @@ export class GetCardsByTypeService {
   {
     const url1: string = `https://deckbuildapi.azurewebsites.net/api/AvailableCardsAPI/GetAllCardByType?type=${type}`;
     const url2: string = `https://localhost:44351/api/AvailableCardsAPI/GetAllCardByType?type=${type}`
-    this.dataArray = this.http.get<ICardModel[]>(url2)
+    this.dataArray = this.http.get<ICardModel[]>(url1)
     return this.dataArray;
   }
 
